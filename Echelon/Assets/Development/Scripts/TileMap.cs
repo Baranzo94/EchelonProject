14e4b814113d07fc44352226a5b6f84fe50cd9ca
Baranzo94 <Liam@Liams-Mac.home>
2016-09-23 12:21:59 +0100

    Project Built

---
+++ Echelon/Assets/Development/Scripts/TileMap.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileMap : MonoBehaviour {
+
+	public Transform tilePrefab;
+	public Vector2 mapSize;
+
+	[Range(0,1)]
+	public float outlinePercent;
+	public float tileSize;
+
+	List<Coord>allTileCoords;
+	Queue<Coord>shuffledTileCoords;
+
+
+	// Use this for initialization
+	void Start () {
+		GenerateMap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void GenerateMap()
+	{
+		allTileCoords = new List<Coord> ();
+
+		for (int x = 0; x < mapSize.x; x++)
+		{
+			for (int y = 0; y < mapSize.y; y++)
+			{
+				allTileCoords.Add(new Coord(x,y));
+			}
+		}
+
+		string holderName = "Map";
+
+		if (transform.FindChild (holderName)) {
+			DestroyImmediate (transform.FindChild (holderName).gameObject);
+		}
+
+		Transform mapHolder = new GameObject (holderName).transform;
+		mapHolder.parent = transform;
+
+		for (int x = 0; x < mapSize.x; x ++)
+		{
+			for (int y = 0; y < mapSize.y; y++) 
+			{
+				Vector3 tilePosition = CoordToPosition (x, y);
+				Transform newTile = Instantiate (tilePrefab, tilePosition, Quaternion.Euler (Vector3.right * 90)) as Transform;
+				newTile.localScale = Vector3.one * (1 - outlinePercent) * tileSize;
+				newTile.parent = mapHolder;
+			}
+		}
+	}
+
+	Vector3 CoordToPosition (int x, int y)
+	{
+		return new Vector3 (-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y) * tileSize;
+	}
+
+	/*public Coord GetRandomCoord()
+	{
+		Coord randomCoord = shuffledTileCoords.Dequeue ();
+
+		shuffledTileCoords.Enqueue (randomCoord);
+
+		return randomCoord;
+	}*/
+
+	public struct Coord
+	{
+		public int x;
+		public int y;
+
+		public Coord(int _x, int _y)
+		{
+			x = _x;
+			y = _y;
+		}
+
+	}
+}
---
+++ Echelon/Assets/Development/Scripts/TowerScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerScript : MonoBehaviour {
+
+
+	public List<GameObject> enemiesSort;
+
+	private float lastFireTime;
+
+	public float fireRate;
+
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+		enemiesSort = new List<GameObject> ();
+		lastFireTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GameObject target = null;
+		//Transform target2 = null;
+
+
+		//float minimalEnemyDistance = float.MaxValue;
+		foreach (GameObject enemy in enemiesSort) {
+			target = enemy;
+			//Calculate distance to objective, attack one closest
+		}
+
+		if (target != null) {
+			
+			if (Time.time - lastFireTime > fireRate) {
+				Fire (target.GetComponent<Collider2D> ());
+				lastFireTime = Time.time;
+			}
+
+			Vector3 direction = gameObject.transform.position - target.transform.position;
+			gameObject.transform.rotation = Quaternion.AngleAxis(
+				Mathf.Atan2 (direction.y, direction.x) * 180 / Mathf.PI,
+				new Vector3 (0, 0, 1));
+		}
+
+
+
+
+		
+	
+	}
+
+	void onEnemyDestroy (GameObject enemy)
+	{
+		enemiesSort.Remove (enemy);
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag.Equals ("Enemy")) {
+			enemiesSort.Add (other.gameObject);
+			//
+		}
+
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.gameObject.tag.Equals ("Enemy")) {
+		
+			enemiesSort.Remove (other.gameObject);
+		}
+	}
+
+	void Fire(Collider2D target)
+	{
+		GameObject ProjectilePrefab = gameObject;
+
+		Vector3 startPosition = gameObject.transform.position;
+		Vector3 targetPosition = target.transform.position;
+		startPosition.z = ProjectilePrefab.transform.position.z;
+		targetPosition.z = ProjectilePrefab.transform.position.z;
+
+
+		/*GameObject newProjectile = (GameObject)Instantiate (ProjectilePrefab);
+		newProjectile.transform.position = startPosition;
+		ProjectileBehavior ProjectileComp = newProjectile.GetComponent<ProjectileBehavior>();
+		ProjectileComp.target = target.gameObject;
+		ProjectileComp.startPosition = startPosition;
+		ProjectileComp.targetPosition = targetPosition;*/
+	}
+}
